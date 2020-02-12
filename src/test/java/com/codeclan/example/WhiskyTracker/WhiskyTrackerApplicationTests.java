da1fdb3cd1e550e31eb73dee48881fb3ce0dcb0e
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-12 21:13:23 +0000

    adds custom queries

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,6 +1,8 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,6 +21,9 @@ public class WhiskyTrackerApplicationTests {
 	@Autowired
 	WhiskyRepository whiskyRepository;
 
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
 	@Test
 	public void contextLoads() {
 	}
@@ -27,6 +32,19 @@ public class WhiskyTrackerApplicationTests {
 	public void canFindWhiskiesYear2018() {
 		List<Whisky> found = whiskyRepository.findByYear(2018);
 		assertEquals(2018, found.get(0).getYear());
+	}
+
+	@Test
+	public void canFindDistilleriesByRegion() {
+		List<Distillery> found = distilleryRepository.findDistilleriesByRegion("Speyside");
+		assertEquals("Speyside", found.get(0).getRegion());
+	}
+
+	@Test
+	public void canFindWhiskiesByDistilleryAndAge() {
+		Distillery distillery1 = distilleryRepository.findDistinctDistilleryByName("Glendronach");
+		List<Whisky> found = whiskyRepository.findWhiskiesByDistilleryAndAge(distillery1, 15);
+		assertEquals("The Glendronach Revival", found.get(0).getName());
 	}
 
 }
